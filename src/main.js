e613f74bf3fb8f2883b64c8f9b5341f753f62173
Szhanpeng <1239897922@qq.com>
2022-04-08 11:17:09 +0800

    实现根据按钮实现页面输出功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//导入ElementUi的主要依赖(全局)
+//import ElementUI from 'element-ui';
+
+//按需导入ElementUi需要的组件
+ import { Button,Radio } from 'element-ui';
+
+//导入ElementUI的样式文件
+import 'element-ui/lib/theme-chalk/index.css';
+
+//引入路由管理
+import router from '../router'
+
 Vue.config.productionTip = false
+//ElementUI注入
+Vue.use(Button)
+Vue.use(Radio)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  //编写时想写成驼峰式需要加上 lintOnSave:false 配置信息
+  lintOnSave:false
 })
